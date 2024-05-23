958cf3914fc1345b69612fc43ed7b593be35ee76
EnderMenderMan <93773937+EnderMenderMan@users.noreply.github.com>
2024-05-23 12:09:23 +0200

    added test code for class BitFunctions

--- TheAmazingGame/Source.cpp
+++ TheAmazingGame/Source.cpp
@@ -1,4 +1,5 @@
 #include "TheAmazingGame.h"
+#include "BitFunctions.h"
 
 int main()
 {
@@ -6,4 +7,14 @@ int main()
 	TheAmazingGame game;
 	game.Play();
 	game.Save();
+
+	/* TestStuff
+	BitFunctions bitFun;
+	unsigned char firt = 0b10101011;
+	unsigned char secound = 0b11011111;
+	bitFun.WriteAllBits(firt);
+	std::cout << " ^\n";
+	bitFun.WriteAllBits(secound);
+	std::cout << "\n\n";  
+	bitFun.WriteAllBits((unsigned char)(firt^secound));*/
 }
